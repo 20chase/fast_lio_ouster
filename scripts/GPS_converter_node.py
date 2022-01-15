3de86ce35763f893aa134e1a143a9e24054fbf69
YinqiangZhang <yinqiang.zhang507@gmail.com>
2022-01-15 22:25:48 +0800

    draw covariance ellipse (circle)

--- scripts/GPS_converter_node.py
+++ scripts/GPS_converter_node.py
@@ -5,12 +5,19 @@ import numpy as np
 from sensor_msgs.msg import NavSatFix, Imu
 from std_srvs.srv import Empty
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
 
 class GPSReceivor():
     def __init__(self, viz_enable=False):
         
         rospy.init_node("gnss_node")
+        
+        # visualization
         self.viz_enable = viz_enable
+        self.confidence_sigma = 2
+        if self.viz_enable:
+            self.init_visualization()
         
         # earth parameters 
         self.a = 6378137.0 # Semi-major axis
@@ -29,6 +36,7 @@ class GPSReceivor():
         
         # result list
         self.path = list()
+        self.ellipse_patch_list = list()
 
         # world to odom pose
         self.t_w2o = (0, 0, 0)
@@ -36,7 +44,7 @@ class GPSReceivor():
         time.sleep(1)
     
     def run(self):
-        loop = rospy.Rate(0.1)
+        loop = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.viz_enable:
                 self.draw_interactive()
@@ -64,16 +72,8 @@ class GPSReceivor():
             reference: https://gssc.esa.int/navipedia/index.php/Transformations_between_ECEF_and_ENU_coordinates
         """
         
-        ref_lon = self.ref_geo_pos.longitude * np.pi /180.0
-        ref_lat = self.ref_geo_pos.latitude * np.pi /180.0
-        
-        ref_R = np.array([[-np.sin(ref_lon), np.cos(ref_lon), 0], 
-                      [-np.sin(ref_lat)*np.cos(ref_lon), -np.sin(ref_lat)*np.sin(ref_lon), np.cos(ref_lat)], 
-                      [np.cos(ref_lat)*np.cos(ref_lon), np.cos(ref_lat)*np.sin(ref_lon), np.sin(ref_lat)]])
-        
         delta_ecef = self.wgs84_to_ecef(geo_pos) - self.ref_ecef
-        
-        enu = np.matmul(ref_R, delta_ecef)
+        enu = np.matmul(self.ref_R, delta_ecef)
         
         return enu
     
@@ -84,16 +84,32 @@ class GPSReceivor():
         if not self.set_initial_position:
             self.ref_geo_pos = msg
             self.ref_ecef = self.wgs84_to_ecef(self.ref_geo_pos)
+            
+            ref_lon = self.ref_geo_pos.longitude * np.pi/180.0
+            ref_lat = self.ref_geo_pos.latitude * np.pi/180.0
+        
+            self.ref_R = np.array([[-np.sin(ref_lon), np.cos(ref_lon), 0], 
+                      [-np.sin(ref_lat)*np.cos(ref_lon), -np.sin(ref_lat)*np.sin(ref_lon), np.cos(ref_lat)], 
+                      [np.cos(ref_lat)*np.cos(ref_lon), np.cos(ref_lat)*np.sin(ref_lon), np.sin(ref_lat)]])
+            
             self.set_initial_position = True
             rospy.loginfo('Set origin successfully!')
         
         current_enu = self.get_enu_position(msg)
-        # rospy.loginfo('E:{},N:{},U:{}'.format(current_enu[0], current_enu[1], current_enu[2]))
+        current_cov = np.resize(np.array(msg.position_covariance), (3, 3))
+        cov_xy = np.sqrt([current_cov[0,0], current_cov[1, 1]])
         
-        self.path.append([current_enu[0], current_enu[1]])
-
         # remove z coordinate
         self.t_w2o = (current_enu[0], current_enu[1], 0.0)
+        
+        if self.viz_enable:
+            self.path.append([self.t_w2o[0], self.t_w2o[1]])
+            # actually, results from GPS are circle
+            patch = Ellipse((self.t_w2o[0], self.t_w2o[1]), 
+                            width=cov_xy[0] * 2 * self.confidence_sigma, 
+                            height=cov_xy[1] * 2 * self.confidence_sigma,
+                            facecolor='g', alpha=0.1)
+            self.ellipse_patch_list.append(patch)
         
     def _reinit_origin(self, srv):
         """
@@ -105,17 +121,22 @@ class GPSReceivor():
         return []
     
     def draw_interactive(self):
-        if len(self.path) != 0: 
-            np_path_list = np.array(self.path)
+        if len(self.ellipse_patch_list)!=0: 
             plt.ion()
-            plt.clf()
-            plt.plot(np_path_list[:, 0], np_path_list[:, 1], color='r', linewidth=1.5)
-            plt.grid(linestyle='--')
-            ax = plt.gca()
-            ax.set_aspect(1)
+            np_path_list = np.array(self.path)
+            self.ax.plot(np_path_list[:, 0], np_path_list[:, 1], color='r', linewidth=1.5)
+            [self.ax.add_patch(patch) for patch in self.ellipse_patch_list]
+            self.ellipse_patch_list = list()
             plt.pause(0.05)
             plt.ioff()
     
+    def init_visualization(self):
+        self.fig = plt.figure(figsize=(8, 8))
+        self.ax = plt.gca()
+        self.ax.grid(linestyle='--')
+        self.ax.set_aspect(1)
+        
+    
 if __name__ == "__main__":
-    GPS_node = GPSReceivor(viz_enable=False)
+    GPS_node = GPSReceivor(viz_enable=True)
     GPS_node.run()
